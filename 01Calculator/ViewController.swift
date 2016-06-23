c732f8b5ef449e951f8707ec08e0f49d71fee604
jfrancop <juliofrp@gmail.com>
2016-06-23 09:26:43 -0700

    first commit

---
+++ 01Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  01Calculator
+//
+//  Created by Julio Franco on 6/22/16.
+//  Copyright © 2016 Julio Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        
+        
+        
+    }
+
+}
+
