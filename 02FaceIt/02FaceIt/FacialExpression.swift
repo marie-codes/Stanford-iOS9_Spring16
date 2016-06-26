fa6b1fd8940559f7a01331de417d4853d43131a4
jfrancop <juliofrp@gmail.com>
2016-06-26 00:30:38 -0700

    end Lec5 - add face model, and UI Gestures

---
+++ 02FaceIt/02FaceIt/FacialExpression.swift
@@ -0,0 +1,51 @@
+//
+//  FacialExpression.swift
+//  02FaceIt
+//
+//  Created by Julio Franco on 6/25/16.
+//  Copyright © 2016 Julio Franco. All rights reserved.
+//
+
+import Foundation
+
+// UI-Independent representation of a facial expression
+
+struct FacialExpression {
+    enum Eyes: Int {
+        case Open
+        case Closed
+        case Squinting
+    }
+    
+    enum EyeBrows: Int {
+        case Relaxed
+        case Normal
+        case Furrowed
+        
+        func moreRelaxedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue - 1) ?? .Relaxed
+        }
+        func moreFurrowedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue + 1) ?? .Furrowed
+        }
+    }
+    
+    enum Mouth: Int {
+        case Frown
+        case Smirk
+        case Neutral
+        case Grin
+        case Smile
+        
+        func sadderMouth() -> Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .Frown
+        }
+        func happierMouth() -> Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .Smile
+        }
+    }
+    
+    var eyes: Eyes
+    var eyeBrows: EyeBrows
+    var mouth: Mouth
+}
