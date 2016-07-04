250e86676039b067cb2c95dd795219a707cbc9dc
jfrancop <juliofrp@gmail.com>
2016-07-04 04:48:18 -0700

    during Lec9 - SmashTag project: create workspace and add prof's Twitter framework

---
+++ 04SmashTag/04SmashTag/TweetTableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  TweetTableViewController.swift
+//  04SmashTag
+//
+//  Created by Julio Franco on 7/3/16.
+//  Copyright © 2016 Julio Franco. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewController: UITableViewController {
+
+    var tweets = [Array<Tweet>]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private func searchForTweets() {
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchText = "#stanford"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+
+}
